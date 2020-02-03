e34be91e587d1a4ca71babc86dab5c25961f310a
Garza0 <konovalovandrey7@gmail.com>
2020-02-03 20:52:35 +0200

    github api connect

--- react-hooks/src/components/Search.js
+++ react-hooks/src/components/Search.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext'
+import { githubReducer } from '../context/github/githubReducer'
 
 export const Search = () => {
   const [value, setValue] = useState('')
   const { show } = useContext(AlertContext)
+  const github = useContext(GithubContext)
 
   const onSubmit = (event) => {
     if (event.key !== 'Enter') {
@@ -11,7 +14,7 @@ export const Search = () => {
     }
 
     if (value.trim()) {
-      console.log('request with:', value)
+      github.search(value.trim())
     } else {
       show('Введите данные пользователя')
     }
