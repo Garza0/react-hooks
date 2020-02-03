ed43a2a73c6ac7332d375d8e16376787c969df73
Garza0 <konovalovandrey7@gmail.com>
2020-02-04 00:48:47 +0200

    build

---
+++ docs/precache-manifest.30a1dc604347355ae49bfbf6cdf321e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93a44a5f96e6fcb5bbd2afb5743ccce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522310f302f0744762e7",
+    "url": "/static/css/main.0c288fef.chunk.css"
+  },
+  {
+    "revision": "988bd3b91191fa0f78ad",
+    "url": "/static/js/2.27ac2e26.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.27ac2e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522310f302f0744762e7",
+    "url": "/static/js/main.9dafb85a.chunk.js"
+  },
+  {
+    "revision": "f7eb8c581961631ba732",
+    "url": "/static/js/runtime-main.de3cd092.js"
+  }
+]);
