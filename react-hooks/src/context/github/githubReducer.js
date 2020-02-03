fe26484083c8e9388c10c7efb29c49f63f6bb81a
Garza0 <konovalovandrey7@gmail.com>
2020-02-03 19:08:16 +0200

    git reducer

---
+++ react-hooks/src/context/github/githubReducer.js
@@ -0,0 +1,14 @@
+const handlers = {
+  [SEARCH_USERS]: (state, payload) => ({ ...state, users: payload, loading: false }),
+  [GET_REPOS]: (state, { payload }) => ({ ...state, repos: payload, loading: false }),
+  [GET_USERS]: (state, { payload }) => ({ ...state, repos: payload, loading: false }),
+  [SET_LOADING]: state => ({ ...state, loading: true }),
+  [CLEAR_USERS]: state => ({ ...state, users: [] })
+  DEFAULT: state => state
+}
+
+
+export const githubReducer = (state, action) => {
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action)
+}
